f996abd8f68bab7983b696d21da07a39ef08ba69
viren <virens88@yahoo.com>
2021-11-29 17:01:38 -0500

    SQL queries and database connecion established

--- index.php
+++ index.php
@@ -4,24 +4,21 @@
         <title>PHP Responses</title>
     </head>
     <body>
-   
-    <!-- https://www.w3schools.com/php/php_mysql_connect.asp -->
+
     <?php
-        //Connect DB
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
+    
+        //helpful db commands to run in SQL to connect your DB to PHP
+        //CREATE USER 'viren'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY 'oooooooo';
+        //GRANT ALL PRIVILEGES ON * . * TO 'viren'@'127.0.0.1';
+        $mysqli = new mysqli('127.0.0.1', 'viren', 'oooooooo', 'STUDENTGRADES');  
+        if ($mysqli->connect_error) 
+        {  
+            die('Connect Error (' . $mysqli->connect_errno . ')');  
+        };  
+          
+        echo 'ok <br>';  
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password);
-
-        // Check connection
-        if ($conn->connect_error)
-        {
-            die("Connection failed: " . $conn->connect_error);
-        } 
-        echo "Connected successfully";
-
+        echo 'hi';
     ?>
     </body>
 </html>
--- insertAndSelectQueries.sql
+++ insertAndSelectQueries.sql
@@ -1,6 +1,15 @@
 -- THIS FILE WILL BE USED TO SAVE THE INSERT AND SELECT STATEMENTS TO INSERT AND RETRIVE GRADES, please refer to the createDB to look at the DB.
 -- requirements:
--- Inserting grades (instert statement)
+-- Inserting grades (insert statement)
 -- Retrieve grades  (select statement )
 
-select grade from courseid
+
+-- insert grade statement: 
+$sql = "INSERT INTO grades (courseid, studentid, term, grade)"
+
+
+
+
+--retrieve grade statement: 
+$sql = "SELECT courseid, studentid, term, grade  FROM grades";
+
