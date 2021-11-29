7ec45b5ea18240fb54667a7e450f8142269742ca
viren <virens88@yahoo.com>
2021-11-29 12:28:34 -0500

    php mysql conn

--- index.php
+++ index.php
@@ -4,10 +4,26 @@
         <title>PHP Responses</title>
     </head>
     <body>
+   
+    <!-- https://www.w3schools.com/php/php_mysql_connect.asp -->
     <?php
+        //Connect DB
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
+
+        // Check connection
+        if ($conn->connect_error)
+        {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+        echo "Connected successfully";
+
         
-        $db = mysqli_connect('localhost','root', 'database_name')
-        or die('Error connecting to MySQL server.');
+
 
     ?>
 
