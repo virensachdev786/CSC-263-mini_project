0f380d821613830440323dbbf593a2f0fb396920
viren <virens88@yahoo.com>
2021-11-30 11:28:56 -0500

    Local setup instructions updated

--- README.md
+++ README.md
@@ -16,3 +16,22 @@ the combination of courseid, studentid and term is primary key
 courseid is a foreign key to courses(courseid)
 studentid is a foreign key to students(studentid)
 Note that you do not have to verify if somebody is a valid user, or what role they are in.
+
+LOCAL SETUP INSTRUCTIONS
+1) Create a local folder
+2) Using terminal / git bash navigate inside the folder
+3) run "git init"
+4) run "git pull https://github.com/virensachdev786/CSC-263-mini_project"
+5) Start mySQL database locally "mysql -u "username" -p" to make sure DB works
+6) run "mysql -u "your DB username" -p < createDB.sql"
+7) run "mysql -u "your DB username" -p"
+8) run CREATE USER 'username'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY 'password';
+9) GRANT ALL PRIVILEGES ON * . * TO 'username'@'127.0.0.1';
+10) run "php -S localhost:3306"
+11) go to browser and go to url "localhost:3306/menu.html"
+
+Basic troubleshooting for:
+if port is already in use:
+run "sudo lsof -i:3306"
+run "sudo killall $(lsof -i:3306)"
+run "apachectl restart"
--- createDB.sql
+++ createDB.sql
@@ -20,13 +20,6 @@ CREATE TABLE STUDENTS(
     PRIMARY KEY(studentid)
 );
 
--- dummy inserted data
-insert into STUDENTS values("1", "Sachdev", "Viren", "2000-11-13");
-insert into STUDENTS values("2", "Ansari", "Arham", "2000-11-1");
-insert into STUDENTS values("3", "Pacheo", "Andy", "2000-11-2");
-insert into STUDENTS values("4", "Singh", "Jaskaran", "2000-11-3");
-
-
 CREATE TABLE COURSES(
     courseid INT NOT NULL, 
     coursename VARCHAR(12), 
@@ -34,11 +27,6 @@ CREATE TABLE COURSES(
     PRIMARY KEY(courseid)
 );
 
--- dummy inserted data
-insert into courses values("1", "CSC-171-001", "3");
-insert into courses values("2", "CSC-175-001", "3");
-insert into courses values("3", "CSC-272-001", "3");
-insert into courses values("4", "CSC-344-001", "3");
 
 CREATE TABLE GRADES(
     courseid INT NOT NULL, 
@@ -49,3 +37,18 @@ CREATE TABLE GRADES(
     FOREIGN KEY(courseid) REFERENCES COURSES(courseid),
     FOREIGN KEY(studentid) REFERENCES STUDENTS(studentid)
 );
+
+
+-- dummy inserted data
+
+--STUDENTS table
+insert into STUDENTS values("1", "Sachdev", "Viren", "2000-11-13");
+insert into STUDENTS values("2", "Ansari", "Arham", "2000-11-1");
+insert into STUDENTS values("3", "Pacheo", "Andy", "2000-11-2");
+insert into STUDENTS values("4", "Singh", "Jaskaran", "2000-11-3");
+
+--COURSES TABLE
+insert into courses values("1", "CSC-171-001", "3");
+insert into courses values("2", "CSC-175-001", "3");
+insert into courses values("3", "CSC-272-001", "3");
+insert into courses values("4", "CSC-344-001", "3");
